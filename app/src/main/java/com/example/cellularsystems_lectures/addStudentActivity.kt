6c100f8dd5ec2f27d551fec1c80d1af33f5a9567
edenaharon1 <edenah9@gmail.com>
2024-12-25 16:44:58 +0200

    adding new student logic

--- app/src/main/java/com/example/cellularsystems_lectures/addStudentActivity.kt
+++ app/src/main/java/com/example/cellularsystems_lectures/addStudentActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class addStudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,18 @@ class addStudentActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        }
+        val saveButton : Button = findViewById(R.id.add_student_activity_save_button)
+        val cancelButton : Button = findViewById(R.id.add_student_activity_cancel_button)
+
+        val nameEditTextv: EditText = findViewById(R.id.add_student_activity_name_edit_text)
+        val idEditText: EditText = findViewById(R.id.add_student_activity_id_edit_text)
+
+        val saveMassageEditView: TextView = findViewById(R.id.add_student_activity_save_button)
+
+        cancelButton.setOnClickListener{
+            saveMassageEditView.text = "Name:${nameEditTextv.text} ID: ${idEditText.text} is saved!"
         }
     }
 }
