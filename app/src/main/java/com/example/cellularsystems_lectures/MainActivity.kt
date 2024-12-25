7d09823e6eecee53f9f8a29b01d2e0314b64ab2b
edenaharon1 <edenah9@gmail.com>
2024-12-25 12:12:46 +0200

    initial  commit

---
+++ app/src/main/java/com/example/cellularsystems_lectures/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cellularsystems_lectures
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    //TODO: step 1: add student button
+    //TODO: step 2: navigate to addStudentActivity
+    //TODO: step 3: create addStudent layout
+    //TODO: step 4: save studentO
+}
